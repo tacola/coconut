fc6e77838b0a995067589d31706d294dbf16e583
tacola <jonbates@163.com>
2013-05-30 00:07:54 +0800

    init
    init version of coconut

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Coconut::Application.config.secret_token = 'bccbb434edb1645b7519ed8a237accd7a666c327785b40691949d6ac191a347ade30cd6eb0821dbd788be202270267aa39544fb6b41a0087f1916d4025dad91c'
